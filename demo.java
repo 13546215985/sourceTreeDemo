91e75a821599c68f7c8f6f79fd9c8ef45c6557fd
ccc <2331420862@qq.com>
2020-06-03 18:47:57 +0800

    first commit

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void mian(String args){
+		//System.out.println("B first  HelloWorld");
+	}
+}
+public class HelloWorld{
+	public static void mian(String args){
+		System.out.println("B  HelloWorld");
+	}
+}
+
